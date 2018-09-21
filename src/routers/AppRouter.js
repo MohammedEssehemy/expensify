e67cc9cab8fd9ae332bb6fd06272bd3333636248
Mohammed Essehemy <mohammedessehemy@gmail.com>
2018-09-21 14:08:26 +0200

    test lint

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
-import Header from './../components/Header';
-import ExpenseDashboardPage from './../components/Dashboard';
-import AddExpensePage from './../components/AddExpense';
-import EditExpensePage from './../components/EditExpense';
-import HelpPage from './../components/Help';
-import NotFoundPage from './../components/NotFound';
+import {
+  BrowserRouter, Route, Switch, Link, NavLink,
+} from 'react-router-dom';
+import Header from '../components/Header';
+import ExpenseDashboardPage from '../components/Dashboard';
+import AddExpensePage from '../components/AddExpense';
+import EditExpensePage from '../components/EditExpense';
+import HelpPage from '../components/Help';
+import NotFoundPage from '../components/NotFound';
 
-const AppRouter = ()=>(
+
+const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Header />
       <Switch>
-        <Route path="/" exact={true} component={ExpenseDashboardPage} />
+        <Route path="/" exact component={ExpenseDashboardPage} />
         <Route path="/create" component={AddExpensePage} />
         <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
